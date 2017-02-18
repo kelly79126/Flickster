64408d5e6671b6ce7f7036c6134bd0f0adc2e670
kelly <kelly.chen@beseye.com>
2017-02-18 22:55:47 +0800

    finish required

---
+++ app/src/main/java/com/example/kelly79126/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,76 @@
+package com.example.kelly79126.flickster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.kelly79126.flickster.R;
+import com.example.kelly79126.flickster.movies.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by kelly79126 on 2017/2/18.
+ */
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie>{
+    // View lookup cache
+    private static class ViewHolder {
+        ImageView poster;
+        TextView title;
+        TextView overview;
+    }
+
+    private Context mContext;
+
+    public MovieArrayAdapter(Context context, List<Movie> movies){
+        super(context, android.R.layout.simple_list_item_1, movies);
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder;
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            viewHolder.poster = (ImageView) convertView.findViewById(R.id.iv_poster);
+            viewHolder.poster.setImageResource(0);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.txt_title);
+            viewHolder.overview = (TextView) convertView.findViewById(R.id.txt_overview);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        }else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+
+        int orientation = mContext.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.poster);
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.poster);
+        }
+
+        viewHolder.title.setText(movie.getOriginalTitle());
+        viewHolder.overview.setText(movie.getOverview());
+
+        return convertView;
+    }
+}
